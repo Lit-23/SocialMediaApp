67c13fc6c9f3cc8735c5f94e8a13d17680f6c986
Lit-23 <rommelmarquez143@gmail.com>
2024-01-08 08:58:14 -0800

    added signin api route

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -5,9 +5,11 @@ export const test = (req, res) => {
 };
 
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from "../models/user.model.js";
+import errorHandler from '../utils/errorHandler.js';
 
-// signup new user
+// signup new user functionality
 export const signup = async (req, res, next) => {
   // request body
   const { profilePicture, firstName, lastName ,email, password } = req.body;
@@ -23,4 +25,32 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error)
   }
+};
+
+// signin functionality
+export const signin = async (req, res, next) => {
+  // req.body
+  const { email, password } = req.body;
+
+  try {
+    // verify user
+    const validUser = await User.findOne({ email });
+    if(!validUser) return next(errorHandler(401, 'User not Found!'));
+
+    // verify password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
+    // generate access token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    
+    // sign in expiry date (1day)
+    const expiryDate = new Date(Date.now() + 86400000); 
+
+    // response
+    res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest);
+  } catch (error) {
+    next(error)
+  }  
 };
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup, test } from "../controllers/userControllers.js";
+import { signup, signin, test } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.get('/', test);
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
