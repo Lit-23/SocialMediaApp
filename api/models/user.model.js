f5e95c6928a36d8e06f26869569ef447ea88299a
Lit-23 <rommelmarquez143@gmail.com>
2024-01-08 07:19:54 -0800

    connect to mongodb and added user schema

---
+++ api/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  profilePicture: {
+    type: String,
+    default: "https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg",
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
